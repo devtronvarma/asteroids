4fadcf149d58fea38b44e644a8db3be1c68669da
devtronvarma <devtronvarma@gmail.com>
2026-05-05 14:05:37 -0500

    add basic circleshape and fps

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -18,6 +21,7 @@ def main():
         screen.fill("black")
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
